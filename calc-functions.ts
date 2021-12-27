911daac61bc3b4d2a669be2505a13a5b29a918ce
Adam Ranieri <adam.ranieri@revature.com>
2021-12-27 11:47:57 -0500

    multiply functionality

--- calc-functions.ts
+++ calc-functions.ts
@@ -6,3 +6,7 @@ export function add(num1: number, num2: number): number{
 export function subtract(num1: number, num2: number): number{
     return num1 - num2;
 }
+
+export function multiply(num1:number, num2: number): number{
+    return num1 * num2;
+}
